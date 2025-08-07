ffe4a662137a56569c687cd1ea6049914b1f570d
Shivanand Mishra <shivanandmishra2003@gmail.com>
2025-08-07 20:47:59 +0530

    Configured GitHub Pages deployments

--- weather-mern-final/client/vite.config.js
+++ weather-mern-final/client/vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
+  base: '/weather_app_mern/',  // Set the base path for the application
   plugins: [react()],
   server: { port: 5173 }
 });
+
+
+
